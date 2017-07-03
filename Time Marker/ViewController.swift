a07776c7963ee29ac5f59787fc5b460812aca004
Akhil Sharma <akhilspc@Akhils-Mac-mini.local>
2017-07-02 20:04:00 -0700

    Initial Commit

---
+++ Time Marker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Marker
+//
+//  Created by Akhil Sharma on 7/2/17.
+//  Copyright © 2017 Akhil Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
